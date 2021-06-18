199b5d4b4b42e64630657758c529368040e8aa32
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-06-17 21:48:32 -0300

    28

---
+++ seção 2/aula_028/index_028.js
@@ -0,0 +1,60 @@
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  floor() (method)
+// floor arredonda para baixo não impora o valor que estejá após a ponto
+// let num1 = 9.54578
+// let num2 = Math.floor(num1)
+// console.log(num2)
+// 9
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  ceil() (method)
+// ceil arredonda pra cima, não impora o valor que estejá após a ponto
+// let num1 = 9.54578
+// let num2 = Math.ceil(num1)
+// console.log(num2)
+// 10
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  round() (method)
+// round, se após o ponto estiver abaixo de 5 arredonda pra baixo,
+// se tiver com 5 ou mais arredonda pra cima
+// let num1 = 9.50
+// let num2 = Math.round(num1)
+// console.log(num2)
+// 10
+// =============================================
+// let num1 = 9.49
+// let num2 = Math.round(num1)
+// console.log(num2)
+// 9
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  max() (method)
+// retorna o maior valor
+// console.log(Math.max(1,2,5,43,-38,472,-362,500))
+// 500
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  min() (method)
+// console.log(Math.min(1,2,5,43,-38,472,-362,500))
+// -362
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  random() (method)
+// retorna um número aleatorio entre 0 e 1.
+// o numero 1 não é incluido
+// console.log(Math.random())
+// 0.9750946724415697
+// =============================================
+// const aleatorio = Math.random() * (10 - 5) + 5
+// console.log(aleatorio)
+// 7.082759170011811
+// =============================================
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5)
+// console.log(aleatorio)
+// 9
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//                  pow() (method)
+// const potencia = Math.pow(2, 10)
+// console.log(potencia)
+// 1024
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// console.log(100 / 0)
+// na verdade não tem como fazer essa conta porem no javascript da isso
+// Infinity
+// true
