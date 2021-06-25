51025a6f28d2b54a6488e4955cca9b91762a4527
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-06-24 23:18:05 -0300

    45

---
+++ seção 3/aula_045/index_045.js
@@ -0,0 +1,34 @@
+// const pontuacaoUsuario = 1000
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP')
+// } else {
+//     console.log('Usuário Normal')
+// }
+// Usuário VIP
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//              Operação ternária
+// (condição) ? 'VALOR VERDADEIRO' : 'VALOR FALSO'
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// const pontuacaoUsuario = 1095
+// const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal'
+// console.log(nivelUsuario)
+// Usuário VIP
+
+// explicação narrada
+// ( SE pontuacaoUsuario for >= a 1000 'verdadeiro' : 'falso')
+// no caso pontuacaoUsuario é 1095
+// por isso a condição é verdadeira, então retorna a msg Usuário VIP.
+// ========================================================
+// const pontuacaoUsuario = 500
+// const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal'
+// console.log(nivelUsuario)
+// Usuário Normal
+// ========================================================
+// const pontuacaoUsuario = 500
+// const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal'
+
+// const corUsuario = null
+// const corPadrao = corUsuario || 'Preto'
+
+// console.log(nivelUsuario, corPadrao)
