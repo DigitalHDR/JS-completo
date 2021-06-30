b863ac0e0873360b55baa5ab60872aed682bb529
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-06-30 19:52:32 -0300

    60

---
+++ seção 3/aula_060/index_060.js
@@ -0,0 +1,39 @@
+// function numeroMaior(x, y) {
+//     if (x > y) {
+//         return x
+//     } else {
+//         return y
+//     }
+// }
+
+// console.log(numeroMaior(1,2))
+//?         2
+//todo =====================================================
+// function numeroMaior(x, y) {
+//     if (x > y) return x
+//     return y
+    
+// }
+
+// console.log(numeroMaior(1,2))
+//?         2
+//todo =====================================================
+// function numeroMaior(x, y) {
+//     return x > y ? x : y  
+// }
+
+// console.log(numeroMaior(100,200))
+//?         200
+//todo =====================================================
+// const numeroMaior = (x, y) => {
+//     return x > y ? x : y  
+// }
+
+// console.log(numeroMaior(100,200))
+//?         200
+//todo =====================================================
+const numeroMaior = (x, y) => x > y ? x : y  
+
+console.log(numeroMaior(100,200))
+//?         200
+//todo =====================================================
