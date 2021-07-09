e6a943ea6b964cb2ec3d957953422e7480e90841
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-07-08 21:52:47 -0300

    71

---
+++ seção 4/aula_071/index_071.js
@@ -0,0 +1,62 @@
+// const nome = 'Lucas'
+
+// function falaNome() {
+//     console.log(nome)
+// }
+
+// falaNome()
+//?         Lucas
+//todo =====================================================
+// const nome = 'Lucas'
+
+// function falaNome() {
+//     console.log(nome)
+// }
+
+// function usaFalaNome() {
+//     falaNome()
+// }
+
+// usaFalaNome()
+//?         Lucas
+//todo =====================================================
+// const nome = 'Lucas'
+
+// function falaNome() {
+//     const nome = 'Fernando'
+//     console.log(nome)
+// }
+
+// function usaFalaNome() {
+//     falaNome()
+// }
+
+// usaFalaNome()
+//?         Fernando
+//todo =====================================================
+// const nome = 'Lucas'
+
+// function falaNome() {
+//     console.log(nome)
+// }
+
+// function usaFalaNome() {
+//     const nome = 'Fernando'
+//     falaNome()
+// }
+
+// usaFalaNome()
+//?         Lucas
+//todo =====================================================
+const nome = 'Lucas'
+
+function falaNome() {
+    console.log(nome)
+}
+
+function usaFalaNome() {
+    const nome = 'Fernando'
+    falaNome()
+}
+
+usaFalaNome()
