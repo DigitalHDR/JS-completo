2bcafda3f7f05c0ac84e2d4aad4c4c7d2397676b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-07 17:52:41 -0300

    121

---
+++ seção 8/aula_121/modelo-webpack/src/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable' //internet explorer antigo
+import 'regenerator-runtime/runtime' //para navegadores antigos
+import executa from './modules/promises'
+import './assets/css/style.css';
+
+executa()
