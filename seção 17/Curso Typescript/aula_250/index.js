5346bbbb02a800fb68d6ee3beeaf169d9a2c677d
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-09 01:04:58 -0300

    250

---
+++ seção 17/Curso Typescript/aula_250/index.js
@@ -0,0 +1,8 @@
+// function greet(nome) {
+//     return `Olá ${nome}` 
+// }
+// console.log(greet('Lucas fernando'));
+//? Olá Lucas fernando
+//todo=============================================
+// const nome = 'Lucas'
+//todo=============================================
---
+++ seção 17/Curso Typescript/aula_250/index.ts
@@ -0,0 +1,16 @@
+// function greet(nome) {
+//     return `Olá ${nome}` 
+// }
+
+// console.log(greet('Lucas fernando'));
+//? Olá Lucas fernando
+//todo=============================================
+// const nome = 'Lucas'
+
+// export default nome
+//? isso faz trabalhar com o model mode.
+//? assim o outra arquivo js não entra em colopso com esse
+//todo=============================================
+const nome = 'Lucas'
+
+export default nome
