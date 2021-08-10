ff34054019beb9c646f554193ea4d91169a5a7e5
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-09 21:10:10 -0300

    257

---
+++ seção 18/Curso Typescript parte 2/aula_256/src/index.ts
@@ -0,0 +1,19 @@
+//? arrays
+// let arrayDeNumeros: Array<number> = [1, 2, 3]
+// let arrayDeString: Array<string> = ['a', 's', 'v']
+
+// outra forma.
+// let arrayDeNumeros2: number[] = [1, 2, 3]
+// let arrayDeString2: string[] = ['a', 's', 'v']
+//todo================================================
+// //?objeto
+// let pessoa: { nome: string, idade: number, adulto?: boolean } = {
+//   idade: 30,
+//   nome: 'Lucas',
+// }
+
+// function soma(x: number, y: number): number {
+//   return x + y
+// }
+
+// const soma2: (x: number, y: number) => number = (x, y) => x + y
