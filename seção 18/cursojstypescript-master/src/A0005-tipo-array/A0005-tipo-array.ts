f6c166aac33b74b1bc476a498d8cf52cdf5bf7c3
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-11 23:11:14 -0300

    typescript array

--- seção 18/cursojstypescript-master/src/A0005-tipo-array/A0005-tipo-array.ts
+++ seção 18/cursojstypescript-master/src/A0005-tipo-array/A0005-tipo-array.ts
@@ -1,20 +1,55 @@
 // Array<T> - T[]
-export function multiplicaArgs(...args: Array<number>): number {
-  return args.reduce((ac, valor) => ac * valor, 1);
+// export function multiplicaArgs(...args: Array<number>): number {
+//   return args.reduce((ac, valor) => ac * valor, 1);
+// }
+
+// export function concatenaString(...args: string[]): string {
+//   return args.reduce((ac, valor) => ac + valor);
+// }
+
+// export function toUpperCase(...args: string[]): string[] {
+//   return args.map((valor) => valor.toUpperCase());
+// }
+
+// const result = multiplicaArgs(1, 2, 3);
+// const concatenacao = concatenaString('a', 'b', 'c');
+// const upper = toUpperCase('a', 'b', 'c');
+
+// console.log(result);
+// console.log(concatenacao);
+// console.log(upper);
+//todo=============================================================
+// export function multiplicaArgs(...args: Array<number>): number {
+//   return args.reduce((ac, valor) => ac * valor, 1)
+// }
+
+// export function concatenaStrings(...args: string[]): string {
+//   return args.reduce((ac, valor) => ac + valor)
+// }
+
+// const result = multiplicaArgs(1, 2, 3)
+
+// const concatenacao = concatenaStrings('a', 'b', 'c')
+
+// console.log(result)
+// console.log(concatenacao)
+//todo=============================================================
+export function multiplicaArgs(...args: number[]): number {
+  return args.reduce((ac, valor) => ac * valor, 1)
 }
 
-export function concatenaString(...args: string[]): string {
-  return args.reduce((ac, valor) => ac + valor);
+export function concatenaStrings(...args: string[]): string {
+  return args.reduce((ac, valor) => ac + valor)
 }
 
 export function toUpperCase(...args: string[]): string[] {
-  return args.map((valor) => valor.toUpperCase());
+  return args.map((valor) => valor.toUpperCase())
 }
 
-const result = multiplicaArgs(1, 2, 3);
-const concatenacao = concatenaString('a', 'b', 'c');
-const upper = toUpperCase('a', 'b', 'c');
+const result = multiplicaArgs(1, 2, 3)
+const concatenacao = concatenaStrings('a', 'b', 'c')
+const upper = toUpperCase('a', 'b', 'c')
 
-console.log(result);
-console.log(concatenacao);
-console.log(upper);
+console.log(result)
+console.log(concatenacao)
+console.log(upper)
--- seção 18/cursojstypescript-master/src/A0006-tipo-tuple/A0006-tipo-tuple.ts
+++ seção 18/cursojstypescript-master/src/A0006-tipo-tuple/A0006-tipo-tuple.ts
@@ -1,23 +1,23 @@
 // Tuple
-const dadosCliente1: readonly [number, string] = [1, 'Luiz'];
-const dadosCliente2: [number, string, string] = [1, 'Luiz', 'Miranda'];
-const dadosCliente3: [number, string, string?] = [1, 'Luiz'];
-const dadosCliente4: [number, string, ...string[]] = [1, 'Luiz', 'Miranda'];
+const dadosCliente1: readonly [number, string] = [1, 'Luiz']
+const dadosCliente2: [number, string, string] = [1, 'Luiz', 'Miranda']
+const dadosCliente3: [number, string, string?] = [1, 'Luiz']
+const dadosCliente4: [number, string, ...string[]] = [1, 'Luiz', 'Miranda']
 
 // dadosCliente1[0] = 100;
 // dadosCliente1[1] = 'Carlos';
 
-console.log(dadosCliente1);
-console.log(dadosCliente2);
-console.log(dadosCliente3);
-console.log(dadosCliente4);
+console.log(dadosCliente1)
+console.log(dadosCliente2)
+console.log(dadosCliente3)
+console.log(dadosCliente4)
 
 // readonly array
-const array1: readonly string[] = ['Luiz', 'Otávio'];
-const array2: ReadonlyArray<string> = ['Luiz', 'Otávio'];
+const array1: readonly string[] = ['Luiz', 'Otávio']
+const array2: ReadonlyArray<string> = ['Luiz', 'Otávio']
 
-console.log(array1);
-console.log(array2);
+console.log(array1)
+console.log(array2)
 
 // Module mode
-export default 1;
+export default 1
