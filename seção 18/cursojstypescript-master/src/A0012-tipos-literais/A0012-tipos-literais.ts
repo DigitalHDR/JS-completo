d93944126303406b9dee15eac8cb852e156e6a14
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-13 13:34:57 -0300

    tipos literais

--- seção 18/cursojstypescript-master/src/A0012-tipos-literais/A0012-tipos-literais.ts
+++ seção 18/cursojstypescript-master/src/A0012-tipos-literais/A0012-tipos-literais.ts
@@ -1,14 +1,31 @@
-let x = 10; // eslint-disable-line
-x = 0b1010;
-const y = 10;
-const a = 100; // eslint-disable-line
+// let x = 10;
+// x = 0b1010;
+// const y = 10;
+// const a = 100;
+
+// const pessoa = {
+//   nome: 'Luiz' as const,
+//   sobrenome: 'Miranda',
+// };
+
+// export function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
+//   return cor;
+// }
+// console.log(escolhaCor('Vermelho'), pessoa, x, y);
+//todo =====================================================
+let x = 10 // eslint-disable-line
+const y = 10
+const a = 100
 
 const pessoa = {
-  nome: 'Luiz' as const,
+  nome: 'Lucas' as const,
   sobrenome: 'Miranda',
-};
-
-export function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
-  return cor;
 }
-console.log(escolhaCor('Vermelho'), pessoa, x, y);
+
+function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
+  return cor
+}
+
+console.log(escolhaCor('Amarelo'))
+
+export default 1
