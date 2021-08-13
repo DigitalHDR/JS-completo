313d9586f0670c0f9db674d6f2a9a08c857f810f
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-13 14:33:24 -0300

    intersecao de type

--- seção 18/cursojstypescript-master/src/A0014-intersection-types/A0014-intersection-types.ts
+++ seção 18/cursojstypescript-master/src/A0014-intersection-types/A0014-intersection-types.ts
@@ -1,20 +1,54 @@
-type TemNome = { nome: string };
-type TemSobrenome = { sobrenome: string };
-type TemIdade = { idade: number };
-type Pessoa = TemNome & TemSobrenome & TemIdade; // AND
+// type TemNome = { nome: string };
+// type TemSobrenome = { sobrenome: string };
+// type TemIdade = { idade: number };
+// type Pessoa = TemNome & TemSobrenome & TemIdade; // AND
 
-type AB = 'A' | 'B';
-type AC = 'A' | 'C';
-type AD = 'D' | 'A';
-type Intersecao = AB & AC & AD;
+// type AB = 'A' | 'B';
+// type AC = 'A' | 'C';
+// type AD = 'D' | 'A';
+// type Intersecao = AB & AC & AD;
+
+// const pessoa: Pessoa = {
+//   nome: 'Luiz',
+//   sobrenome: 'Miranda',
+//   idade: 30,
+// };
+
+// console.log(pessoa);
+
+// // Module mode
+// export { pessoa };
+//todo =====================================================
+// & é igual a and
+
+// type TemNome = { nome: string }
+// type TemSobrenome = { sobrenome: string }
+// type TemIdade = { idade: number }
+// type Pessoa = TemNome & TemSobrenome & TemIdade
+
+// const pessoa: Pessoa = {
+//   nome: 'Lucas',
+//   sobrenome: 'Fernando',
+//   idade: 32,
+// }
+
+// console.log(pessoa)
+//?     { nome: 'Lucas', sobrenome: 'Fernando', idade: 32 }
+//todo =====================================================
+type TemNome = { nome: string }
+type TemSobrenome = { sobrenome: string }
+type TemIdade = { idade: number }
+type Pessoa = TemNome & TemSobrenome & TemIdade
+
+type AB = 'A' | 'B'
+type AC = 'A' | 'C'
+type AD = 'D' | 'A'
+type intersecao = AB & AC & AD
 
 const pessoa: Pessoa = {
-  nome: 'Luiz',
-  sobrenome: 'Miranda',
-  idade: 30,
-};
+  nome: 'Lucas',
+  sobrenome: 'Fernando',
+  idade: 32,
+}
 
-console.log(pessoa);
-
-// Module mode
-export { pessoa };
+console.log(pessoa)
