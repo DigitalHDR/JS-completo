2f2fbc6890e6afde2b5ee1f5b16a9b07bd3c7c8b
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-13 00:29:54 -0300

    união de tipagem

--- seção 18/cursojstypescript-master/src/A0011-union-types/A0011-union-types.ts
+++ seção 18/cursojstypescript-master/src/A0011-union-types/A0011-union-types.ts
@@ -1,16 +1,73 @@
-function addOrConcat(
-  a: number | string | boolean,
-  b: number | string | boolean,
-): number | string {
-  if (typeof a === 'number' && typeof b === 'number') return a + b;
-  return `${a}${b}`;
+// function addOrConcat(
+//   a: number | string | boolean,
+//   b: number | string | boolean,
+// ): number | string {
+//   if (typeof a === 'number' && typeof b === 'number') return a + b;
+//   return `${a}${b}`;
+// }
+
+// console.log(addOrConcat(10, 20));
+// console.log(addOrConcat('10', '20'));
+// console.log(addOrConcat(10, '20'));
+// console.log(addOrConcat('10', 20));
+// console.log(addOrConcat(true, true));
+
+//todo =====================================================
+
+// function add(a: number, b: number): number {
+//   return a + b
+// }
+
+// console.log(add(10, 20))
+//?     30
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*                    |  |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// function addOrConcat(a: number | string, b: number | string) {
+//   if (typeof a === 'number' && typeof b === 'number') return a + b
+//   if (typeof a === 'string' && typeof b === 'string') return a + b
+// }
+
+// console.log(addOrConcat(10, 20))
+// console.log(addOrConcat('10', '20'))
+//?     30
+//?     1020
+//todo =====================================================
+// function addOrConcat(a: number | string, b: number | string) {
+//   if (typeof a === 'number' && typeof b === 'number') return a + b
+//   return `${a}${b}`
+// }
+
+// console.log(addOrConcat(10, 20))
+// console.log(addOrConcat('10', '20'))
+//?     30
+//?     1020
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*       | expecificando a fuçao number | string |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+// function addOrConcat(a: number | string, b: number | string): number | string {
+//   if (typeof a === 'number' && typeof b === 'number') return a + b
+//   return `${a}${b}`
+// }
+
+// console.log(addOrConcat(10, 20))
+// console.log(addOrConcat('10', '20'))
+//?     30
+//?     1020
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*                    |  |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function addOrConcat(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b
+  return `${a}${b}`
 }
 
-console.log(addOrConcat(10, 20));
-console.log(addOrConcat('10', '20'));
-console.log(addOrConcat(10, '20'));
-console.log(addOrConcat('10', 20));
-console.log(addOrConcat(true, true));
-
-// Module mode
-export default 1;
+console.log(addOrConcat(10, 20))
+console.log(addOrConcat('10', '20'))
+console.log(addOrConcat(10, '20'))
+//?     30
+//?     1020
+//?     1020
+//todo =====================================================
