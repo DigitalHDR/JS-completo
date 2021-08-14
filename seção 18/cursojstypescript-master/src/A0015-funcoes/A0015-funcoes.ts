a7c75dfa0dbf87b2860b8097442c6c1b7f379a7e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-14 16:21:53 -0300

    structural type

--- seção 18/cursojstypescript-master/src/A0015-funcoes/A0015-funcoes.ts
+++ seção 18/cursojstypescript-master/src/A0015-funcoes/A0015-funcoes.ts
@@ -1,20 +1,77 @@
-type MapStringsCallback = (item: string) => string;
+// type MapStringsCallback = (item: string) => string;
+
+// export function mapStrings(
+//   array: string[],
+//   callbackfn: MapStringsCallback,
+// ): string[] {
+//   const newArray: string[] = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     const item = array[i];
+//     newArray.push(callbackfn(item));
+//   }
+
+//   return newArray;
+// }
+
+// const abc = ['a', 'b', 'c'];
+// const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
+// console.log(abc);
+// console.log(abcMapped);
+//todo =====================================================
+// function mapStrings(array: string[], callbackfn: CallableFunction): string[] {
+//   const newArray: string[] = []
+
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(callbackfn(array[i]))
+//   }
+
+//   return newArray
+// }
+
+// const abc = ['a', 'b', 'c']
+// const abcMapped = mapStrings(abc, function (item: any) {
+//   return item.toUpperCase()
+// })
+// console.log(abcMapped)
+//?     [ 'A', 'B', 'C' ]
+//todo =====================================================
+// function mapStrings(array: string[], callbackfn: CallableFunction): string[] {
+//   const newArray: string[] = []
+
+//   for (let i = 0; i < array.length; i++) {
+//     newArray.push(callbackfn(array[i]))
+//   }
+
+//   return newArray
+// }
+
+// const abc = ['a', 'b', 'c']
+// const abcMapped = mapStrings(abc, function (item: any) {
+//   return item.toUpperCase()
+// })
+// console.log(abc)
+// console.log(abcMapped)
+//?     [ 'a', 'b', 'c' ]
+//?     [ 'A', 'B', 'C' ]
+//todo =====================================================
+type MapStringsCallback = (item: string) => string
 
 export function mapStrings(
   array: string[],
   callbackfn: MapStringsCallback,
 ): string[] {
-  const newArray: string[] = [];
+  const newArray: string[] = []
 
   for (let i = 0; i < array.length; i++) {
-    const item = array[i];
-    newArray.push(callbackfn(item));
+    const item = array[i]
+    newArray.push(callbackfn(array[i]))
   }
 
-  return newArray;
+  return newArray
 }
 
-const abc = ['a', 'b', 'c'];
-const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
-console.log(abc);
-console.log(abcMapped);
+const abc = ['a', 'b', 'c']
+const abcMapped = mapStrings(abc, (item) => item.toUpperCase())
+console.log(abc)
+console.log(abcMapped)
