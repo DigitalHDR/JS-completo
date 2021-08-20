8fc6f4c0b633f7d90649f4cb9beabeb6b1cc9726
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-20 20:42:32 -0300

    132

--- seção 10 Node Express MongoDB/aula_132/server.js
+++ seção 10 Node Express MongoDB/aula_132/server.js
@@ -9,8 +9,42 @@
 //?     http://meusite.com/sobre <- GET -> entregue a página /sobre
 //?     http://meusite.com/contato <- GET -> entregue a página /contato
 //todo =====================================================
+// http://facebook.com/profiles/12345?campanha=googleads&nome_campanha=seila
+//?     ? signigica que está no começo, que está iniciando uma query string
+//?     & significa que está continuando o query
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send(`
+//     <form action="/" method="POST">
+//     Nome do cliente: <input type="text" name="nome">
+//     <button>Olá mundo</button>
+//     </form>
+//     `)
+// })
+
+// app.get('/testes/:idUsuarios?/:parametro?/:outraCoisa', (req, res) => {
+//     console.log(req.params)
+//     res.send(req.params.idUsuarios)
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('Recebi o formulário')
+// })
+
+// app.listen(3000, () => {
+//     console.log('Acessa http://localhost:3000')
+//     console.log('Servidor express com nodejs rodando')
+// })
+
+//?     { idUsuarios: '123', parametro: 'fdsgfd', outraCoisa: 'aff' }
+//todo =====================================================
 const express = require('express')
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send(`
@@ -21,12 +55,15 @@ app.get('/', (req, res) => {
     `)
 })
 
-app.post('/', (req, res) => {   //? vai aparecer isso depois de app.get
-    res.send('Recebi o formulário')
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send(req.query.facebookprofile)
 })
 
-app.get('/contato', (req, res) => {
-    res.send('Obrigado por entrar em contato com a gente')
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send(`O que você me enviou foi: ${req.body.nome}`) //? name é o mesmo do input acima em name="nome"
 })
 
 app.listen(3000, () => {
