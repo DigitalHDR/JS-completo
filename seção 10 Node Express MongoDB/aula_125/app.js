72948b85099b87c44edff45cd1fb17db11901d1e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-20 16:34:57 -0300

    128

---
+++ seção 10 Node Express MongoDB/aula_125/app.js
@@ -0,0 +1,18 @@
+// const mutiplicacao = require('./mod1')
+
+// console.log(mutiplicacao(3, 3))
+//?         9
+//todo =====================================================
+// const Cachorro = require('./mod1')
+
+// const Cachorro1 = new Cachorro('Pitoco')
+// Cachorro1.latir()
+//?         Pitoco está fazendo au au
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//*          | mosrtar o caminho do arquivo e da pata |
+//* %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// console.log(__filename)
+// console.log(__dirname)
+//?         c:\JS completo\seção 10 Node Express MongoDB\aula_125\app.js
+//?         c:\JS completo\seção 10 Node Express MongoDB\aula_125
+//todo =====================================================
---
+++ seção 10 Node Express MongoDB/aula_128/index.js
@@ -0,0 +1,25 @@
+// const fs = require('fs').promises
+// const path = require('path')
+
+// fs.readdir(path.resolve(__dirname))
+//     .then(files => console.log(files))
+//     .catch(e => console.log(e))
+//?         [ 'index.js' ]
+//todo =====================================================
+// const fs = require('fs').promises;
+// const path = require('path');
+
+// async function readdir(rootDir) {
+//     rootDir = rootDir || path.resolve(__dirname);
+//     const files = await fs.readdir(rootDir);
+//     walk(files);
+// }
+
+// function walk(files) {
+//     for (let file of files) {
+//         console.log(file);
+//     }
+// }
+
+// readdir('\JS completo\seção 10 Node Express MongoDB\aula_128');
+//! PAUSADO O COMINHO NÃO DEU CERTO;
