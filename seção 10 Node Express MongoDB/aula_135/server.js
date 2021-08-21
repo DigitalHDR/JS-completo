c6aacf6c39bde1bf3e15c92ae8beca466e274d11
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-20 22:42:55 -0300

    135

---
+++ seção 10 Node Express MongoDB/aula_135/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.relative(__dirname, 'public')))
+
+//! ########################################################
+//? caminho absoluto
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+
+//? caminha relativo
+// app.set('views', './src/views')
+//! ########################################################
+
+app.set('view engine', 'ejs')
+
+app.use(routes)
+
+app.listen(3000, () => {
+    console.log('Acessa http://localhost:3000')
+    console.log('Servidor express com nodejs rodando')
+})
