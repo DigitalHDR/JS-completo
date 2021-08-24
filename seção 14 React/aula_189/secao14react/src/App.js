7d039f945688524ec367b3f84bce9c9e3d2dd36d
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-23 23:53:02 -0300

    189

--- seção 14 React/aula_189/secao14react/src/App.js
+++ seção 14 React/aula_189/secao14react/src/App.js
@@ -1,26 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>-_-</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <h1>Alguma coisa</h1>
 }
-
-export default App;
